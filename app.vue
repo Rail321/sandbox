68962eac37ef9cc0dadd09c15684897f26ac4da5
Rail Shakurov <shakurov-98@mail.ru>
2022-12-21 22:12:50 +0300

    +add

--- app.vue
+++ app.vue
@@ -1,28 +1,88 @@
 <template>
-  <div>
-    <svg viewBox="0 0 10 10" class="svg-5">
-      <path d="M2,2 L8,2 L2,5 L8,5 L2,8 L8,8" />
-    </svg>
+  <div class="wrapper">
+    <div class="container">
+      <svg class="chart"
+        v-bind:viewBox="viewBox"
+        v-on:click="toggle"
+      >
+        <path class="path"
+          v-bind:d="pathed"
+        />
+      </svg>
+    </div>
   </div>
 </template>
 
+<script setup>
+  const modelValue = ref( [] )
+  const padding = ref( 10 )
+  const width = ref( 300 )
+  const height = ref( 200 )
+  const viewBox = computed( () => {
+    return `0 0 ${ width.value } ${ height.value }`
+  } )
+  const paddinged = computed( () => {
+    const result = []
+    modelValue.value.forEach( el => {
+      result.push( {
+        x: ( padding.value + ( ( width.value - ( 2 * padding.value ) ) * ( el.x / width.value ) ) ),
+        y: ( padding.value + ( ( height.value - ( 2 * padding.value ) ) * ( el.y / height.value ) ) )
+      } )
+    } )
+    return result
+  } )
+  const pathed = computed( () => {
+    const segments = []
+    paddinged.value.forEach( ( el, idx ) => {
+      if ( idx === 0 ) return segments.push( `M${ el.x },${ el.y }` )
+      segments.push( `L${ el.x },${ el.y }` )
+    } )
+    const result = segments.join( ' ' )
+    return result
+  } )
+
+  const data = ref( [
+    [ { x: 0, y: 0 }, { x: 150, y: 200 }, { x: 300, y: 0 } ],
+    [ { x: 0, y: 200 }, { x: 150, y: 0 }, { x: 300, y: 200 } ],
+    [ { x: 0, y: 0 }, { x: 75, y: 200 }, { x: 150, y: 0 }, { x: 225, y: 200 }, { x: 300, y: 0 } ],
+    [ { x: 0, y: 200 }, { x: 75, y: 0 }, { x: 150, y: 200 }, { x: 225, y: 0 }, { x: 300, y: 200 } ]
+  ] )
+  modelValue.value = data.value[ 0 ]
+  const toggle = () => {
+    const index = data.value.findIndex( el => ( el === modelValue.value ) )
+    modelValue.value = data.value[ ( ( ( index + 1 ) === data.value.length ) ? 0 : ( index + 1 ) ) ]
+  }
+</script>
+
 <style>
-  .svg-5:hover path {
-    d: path("M3,3 L8,3 L2,5 L8,5 L3,7 L7,7");
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
-  svg {
-    width: 30%;
-    height: 30%;
-    background: #eee;
-    margin-bottom: 5%;
+  html, body {
+    height: 100%;
   }
-  svg polyline,
-  svg line,
-  svg path {
+  .wrapper {
+    padding: 10px;
+  }
+  .container {
+    width: 100%;
+    max-width: 300px;
+    margin: 0 auto;
+  }
+  .chart {
+    width: 100%;
+    height: 200px;
+    display: block;
+    transform: scaleY(-1);
+    background-color: gainsboro;
+  }
+  .path {
     fill: none;
     stroke: #000;
+    stroke-width: 5;
     stroke-linecap: round;
-    stroke-linejoin: round;
-    transition: 0.2s;
+    transition: .1s;
   }
 </style>
