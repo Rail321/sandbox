29c382301af5da7cd20ddf1906862e1923123802
Rail <shakurov-98@mail.ru>
2022-12-23 14:41:57 +0300

    +add

--- app.vue
+++ app.vue
@@ -1,3 +1,43 @@
 <template>
-  <div/>
+  <div>
+    <div>
+      <base-checkbox
+        v-model="value1"
+      />
+      <span>-</span>
+      <span>&nbsp;</span>
+      <span>{{ value1 }}</span>
+      <button type="button"
+        v-on:click="value1 = !value1"
+      >Переключить</button>
+    </div>
+
+    <div>
+      <base-checkbox
+        v-model="value2"
+      />
+      <span>-</span>
+      <span>&nbsp;</span>
+      <span>{{ value2 }}</span>
+      <button type="button"
+        v-on:click="value2 = !value2"
+      >Переключить</button>
+    </div>
+
+    <div>
+      <base-checkbox/>
+      <span>-</span>
+      <span>&nbsp;</span>
+      <span>{{ value3 }}</span>
+      <button type="button"
+        v-on:click="value3 = !value3"
+      >Переключить</button>
+    </div>
+  </div>
 </template>
+
+<script setup>
+  const value1 = ref( false )
+  const value2 = ref( true )
+  const value3 = ref( false )
+</script>
---
+++ components/Base/Checkbox.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    type="checkbox"
+    v-bind:checked="modelValue"
+    v-on:change="onChange"
+  />
+</template>
+
+<script setup>
+  const instance = getCurrentInstance()
+
+  const props = defineProps( {
+    modelValue: {
+      type: Boolean,
+      default: () => false
+    }
+  } )
+
+  const emit = defineEmits( [
+    'update:modelValue'
+  ] )
+
+  const {
+    modelValue
+  } = toRefs( props )
+
+  const onChange = event => {
+    const value = event.target.checked
+    emit( 'update:modelValue', value )
+    instance.proxy.$forceUpdate()
+  }
+</script>
