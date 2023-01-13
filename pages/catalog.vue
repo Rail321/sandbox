e94c98d87d3862e10bc45321d137c96ad00761a6
Rail <shakurov-98@mail.ru>
2023-01-13 12:52:49 +0300

    +add

---
+++ pages/catalog.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      <nuxt-link style="margin-right: 10px;" to="/">Главная</nuxt-link>
+      <nuxt-link to="/catalog">Каталог</nuxt-link>
+    </div>
+    <div>
+      <div
+        v-if="loading"
+      >
+        <span>Загрузка...</span>
+      </div>
+      <div
+        v-else
+      >
+        <h1>Каталог</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const loading = useState( 'loading', () => false )
+  await new Promise( resolve => setTimeout( resolve, 3000 ) )
+  onBeforeRouteLeave(
+    () => ( loading.value = true )
+  )
+  loading.value = false
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <nuxt-link style="margin-right: 10px;" to="/">Главная</nuxt-link>
+      <nuxt-link to="/catalog">Каталог</nuxt-link>
+    </div>
+    <div>
+      <div
+        v-if="loading"
+      >
+        <span>Загрузка...</span>
+      </div>
+      <div
+        v-else
+      >
+        <h1>Главная</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const loading = useState( 'loading', () => false )
+  try {
+    await fetch( 'https://jsonplaceholder.typicode.com/todos' )
+  } catch {}
+  onBeforeRouteLeave(
+    () => ( loading.value = true )
+  )
+  loading.value = false
+</script>
